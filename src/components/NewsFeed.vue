5fbc215867dadd2df59054fbd4846473cfd82a67
CSE200 <shubhampandey5600@gmil.com>
2025-04-18 15:45:37 +0530

    switching shorts and home

--- src/components/NewsFeed.vue
+++ src/components/NewsFeed.vue
@@ -1,5 +1,10 @@
+<script setup>
+import FeaturedNews from '@/components/FeaturedNews.vue'
+import NewsCard from '@/components/NewsCard.vue'
+import newsList from '@/data/news.json'
+</script>
+
 <template>
-  <!-- <div class="relative space-y-2 mx-auto"> -->
   <div class="relative space-y-2 mx-auto px-2 sm:px-4 md:px-6 lg:px-8">
     <router-link :to="`/news/${newsList[0].id}`">
       <FeaturedNews :article="newsList[0]" />
@@ -15,15 +20,5 @@
         <NewsCard :article="article" />
       </router-link>
     </div>
-    <div class="absolute top-30 right-1 left-0 bottom-1 flex justify-center items-center p-4">
-      <Button />
-    </div>
   </div>
 </template>
-
-<script setup>
-import FeaturedNews from '@/components/FeaturedNews.vue'
-import NewsCard from '@/components/NewsCard.vue'
-import newsList from '@/data/news.json'
-import Button from './Button.vue'
-</script>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -42,25 +42,3 @@
     </div>
   </nav>
 </template>
-
-<!-- <script setup></script>
-
-<template>
-  <nav
-    class="hidden xl:block h-screen border-r border-black border-opacity-20 p-4 space-y-8 min-w-[250px]"
-  >
-    <div class="">
-      <div class="h-[50px] w-[100px]"><img src="../assets/logo.png" /></div>
-    </div>
-
-    <div class="flex flex-col items-start space-y-8">
-      <button>टॉप न्यूज़</button>
-      <button>राज्य - शहर</button>
-      <button>एंटरटेनमेंट</button>
-      <button>बॉलीवुड</button>
-      <button>स्पोर्ट्स</button>
-      <button>इंटरनेशनल</button>
-      <button>रेसिपी</button>
-    </div>
-  </nav>
-</template> -->
