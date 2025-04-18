0562b0011c8c4ae999bd2e35318e66a231a87771
Priyanshucse25 <143019452+Priyanshucse25@users.noreply.github.com>
2025-04-18 15:44:19 +0530

    changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,14 @@
-<script setup></script>
+<script setup>
+const categories = [
+  { icon: '../assets/images/fire.svg', label: 'टॉप न्यूज़' },
+  { icon: '../assets/images/rajya-sahar 1.svg', label: 'राज्य - शहर' },
+  { icon: '../assets/images/entertainment 1.svg', label: 'एंटरटेनमेंट' },
+  { icon: '../assets/images/bollywood 1.svg', label: 'बॉलीवुड' },
+  { icon: '../assets/images/sports 1.svg', label: 'स्पोर्ट्स' },
+  { icon: '../assets/images/international 1.svg', label: 'इंटरनेशनल' },
+  { icon: '../assets/images/receipe 1.svg', label: 'रेसिपी' },
+]
+</script>
 
 <template>
   <nav class="block lg:hidden sticky top-0 z-0">
@@ -16,40 +26,19 @@
           class="w-6 h-6 sm:w-4 sm:h-4"
         />
         <span class="text-xs sm:text-sm md:text-base font-medium text-[#4D4D4D] whitespace-nowrap">
-  Join Our Group
-</span>
-
+          Join Our Group
+        </span>
       </div>
     </div>
 
     <div class="flex whitespace-nowrap w-full px-4 space-x-8 overflow-x-auto">
-      <button class="flex items-center space-x-2">
-        <img src="../assets/images/fire.svg" class="w-5 h-5" />
-        <span>टॉप न्यूज़</span>
-      </button>
-      <button class="flex items-center space-x-2">
-        <img src="../assets/images/rajya-sahar 1.svg" class="w-5 h-5" />
-        <span>राज्य - शहर</span>
-      </button>
-      <button class="flex items-center space-x-2">
-        <img src="../assets/images/entertainment 1.svg" class="w-5 h-5" />
-        <span>एंटरटेनमेंट</span>
-      </button>
-      <button class="flex items-center space-x-2">
-        <img src="../assets/images/bollywood 1.svg" class="w-5 h-5" />
-        <span>बॉलीवुड</span>
-      </button>
-      <button class="flex items-center space-x-2">
-        <img src="../assets/images/sports 1.svg" class="w-5 h-5" />
-        <span>स्पोर्ट्स</span>
-      </button>
-      <button class="flex items-center space-x-2">
-        <img src="../assets/images/international 1.svg" class="w-5 h-5" />
-        <span>इंटरनेशनल</span>
-      </button>
-      <button class="flex items-center space-x-2">
-        <img src="../assets/images/receipe 1.svg" class="w-5 h-5" />
-        <span>रेसिपी</span>
+      <button
+        v-for="(cat, index) in categories"
+        :key="index"
+        class="flex items-center space-x-2"
+      >
+        <img :src="cat.icon" class="w-5 h-5" />
+        <span>{{ cat.label }}</span>
       </button>
     </div>
   </nav>
