bf28d1822d8e937ae3e8e8ca6183b090d90059c7
CSE200 <shubhampandey5600@gmil.com>
2025-05-02 18:24:16 +0530

    FeaturedNews Change

--- src/stores/FeaturedNewsStore.js
+++ src/stores/FeaturedNewsStore.js
@@ -1,12 +1,12 @@
 import { defineStore } from 'pinia'
 
 export const useFeaturedNewsStore = defineStore('featuredNews', () => {
-  // const icons = [{ name: 'pi pi-whatsapp' }, { name: 'pi pi-facebook' }, { name: 'pi pi-link' }]
-  const icons = ref([
-    { name: 'pi pi-whatsapp', link: 'https://wa.me/your-phone-number' },
-    { name: 'pi pi-facebook', link: 'https://facebook.com/your-profile' },
-    { name: 'pi pi-link', link: 'https://your-link.com' },
-  ])
+  const icons = [{ name: 'pi pi-whatsapp' }, { name: 'pi pi-facebook' }, { name: 'pi pi-link' }]
+  // const icons = ref([
+  //   { name: 'pi pi-whatsapp', link: 'https://wa.me/your-phone-number' },
+  //   { name: 'pi pi-facebook', link: 'https://facebook.com/your-profile' },
+  //   { name: 'pi pi-link', link: 'https://your-link.com' },
+  // ])
 
   const formatDate = (dateStr) => {
     const date = new Date(dateStr)
